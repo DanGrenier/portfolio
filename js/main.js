18cc129ce1aadc0f7b35fbaa0369e3ae4f6c599a
Daniel Grenier <dgrenier@smallbizpros.com>
2018-04-19 16:25:38 -0400

    Added static markup for work examples

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-import 'react-dom'
-console.log("Loaded React Dom");
-console.log("Webpack works");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
+const myWork = [
+{
+  'title': "Proposals",
+  'image': {
+  	'desc': "Screenshot of proposal tool",
+  	'src': "images/example1.png",
+  	'comment': ""
+  }	
+},
+{
+  'title': "Work Example2",
+  'image': {
+  	'desc': "Example screenshot2",
+  	'src': "images/example2.png",
+  	'comment': ""
+  }	
+},
+{
+  'title': "work Example3",
+  'image': {
+  	'desc': "Example screenshot3",
+  	'src': "images/example3.png",
+  	'comment': ""
+  }	
+}
+
+
+]
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
 
 
