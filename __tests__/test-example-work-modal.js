f432663e2a14f72e42a06a5c0afa68898829319d
Daniel Grenier <dgrenier@smallbizpros.com>
2018-05-18 11:38:01 -0400

    Fixed issue in testing for two anchors

--- __tests__/test-example-work-modal.js
+++ __tests__/test-example-work-modal.js
@@ -8,6 +8,7 @@ Enzyme.configure({ adapter: new Adapter()});
 const myExample = {
   'title': "Proposals",
   'href': "https://example.com",
+  'github': "https://example.com",
   'desc': "lorem ipsim dolor sit amet",
   'image': {
   	'desc': "Screenshot of proposal tool",
@@ -23,12 +24,16 @@ describe("ExampleWorkModal component", () => {
 
 	let anchors = component.find("a");
 
-	it("Should contain a single 'a' element", () => {
-		expect(anchors.length).toEqual(1);
+	it("Should contain two 'a' element", () => {
+		expect(anchors.length).toEqual(2);
 	});
 
 	it("Should link to our project", () => {
-		expect(anchors.prop('href')).toEqual(myExample.href);
+		expect(anchors.first().prop('href')).toEqual(myExample.href);
+	});
+
+	it("Should link to our Github", () => {
+		expect(anchors.at(1).prop('href')).toEqual(myExample.github);
 	});
 
 	it("Should have the modal class set correctly", () =>{
