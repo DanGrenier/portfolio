07fedd970bfb71250356ca19cb838024e9d7d50b
Daniel Grenier <dgrenier@smallbizpros.com>
2018-05-23 13:28:05 -0400

    fixed test cases after style changes

--- __tests__/test-example-work-modal.js
+++ __tests__/test-example-work-modal.js
@@ -37,7 +37,7 @@ describe("ExampleWorkModal component", () => {
 	});
 
 	it("Should have the modal class set correctly", () =>{
-		expect(component.find(".background--skyBlue").hasClass("modal--closed")).toBe(true);
-		expect(openComponent.find(".background--skyBlue").hasClass("modal--open")).toBe(true);
+		expect(component.find(".background--grey").hasClass("modal--closed")).toBe(true);
+		expect(openComponent.find(".background--grey").hasClass("modal--open")).toBe(true);
 	})
 });
